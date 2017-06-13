1efb255f84d706e232c711529ab6a51f1ccfe956
shonnawho <peshonna@gmail.com>
2017-06-12 20:54:02 -0500

    web ceaser update

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, redirect, render_template
+import cgi
+import os
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+@app.route("/")
+def index():
+    return render_template('signup_form.html')
+
+app.run()
+
